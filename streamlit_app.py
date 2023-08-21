7625f572cefed8ed8be9a0b803e75514e5e6a553
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2023-08-21 23:09:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
-    placeholder="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",
+    placeholder="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",
     type="password")
 st.image("socialai.jpg")
 # Path to the CSV file
@@ -26,7 +26,7 @@ data = pd.read_csv(csv_file_path)
 texts = data.astype(str).agg(" ".join, axis=1).tolist()
 
 # Set up OpenAI embeddings
-embeddings = OpenAIEmbeddings(openai_api_key="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")
+embeddings = OpenAIEmbeddings(openai_api_key="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")
 
 # Create vectorstore using FAISS from text data
 vectorstore = FAISS.from_texts(texts, embeddings)
