fe419857d1edbb475a9f23e3fb04d7a716ca2e4a
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2023-08-22 00:52:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from langchain.vectorstores import FAISS
 import tempfile
 import pandas as pd
 
-
+keys_secret = st.secrets["keys"]
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
     placeholder="openai_keyyyy",
@@ -29,7 +29,7 @@ data = pd.read_csv(csv_file_path)
 texts = data.astype(str).agg(" ".join, axis=1).tolist()
 
 # Set up OpenAI embeddings
-embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["key"])
+embeddings = OpenAIEmbeddings(openai_api_key=keys_secret)
 
 # Create vectorstore using FAISS from text data
 vectorstore = FAISS.from_texts(texts, embeddings)
